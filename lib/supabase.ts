9617feac186463ce9cee0df28f73616e0ac96c9d
TK-LPT-847 <>
2026-06-08 12:35:40 +0500

    Debug Supabase env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log(
-  "SUPABASE KEY EXISTS:",
-  !!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+console.log("URL EXISTS:", !!url);
+console.log("KEY EXISTS:", !!key);
+
+export const supabase = createClient(url!, key!);
